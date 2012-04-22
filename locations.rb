54c64338097c9834f0398a15ff8a81f8bdce79f9
Paul R Alexander <palexander@stanford.edu>
2012-04-21 21:09:35 -0700

    Output datetime

--- locations.rb
+++ locations.rb
@@ -4,6 +4,8 @@ require 'open-uri'
 require 'hpricot'
 require 'cgi'
 require 'sinatra'
+require 'parsedate'
+
 
 get "/" do
   if params["guser_xml"].nil?
@@ -17,6 +19,9 @@ get "/" do
 
   locations = []
   doc.search("/feed/entry/content").each do |el|
+    time_match = el.inner_html.match(/When: (.*) to/)[1] rescue nil
+    time = Time.mktime(*ParseDate.parsedate(time_match))
+    next if time.nil?
     loc = el.inner_html.match(/Where: (.*)$/)[1] rescue nil
     next if loc.nil?
     geocode = JSON.parse(open("http://maps.googleapis.com/maps/api/geocode/json?address=#{CGI.escape(loc)}&sensor=false").read)
@@ -25,7 +30,7 @@ get "/" do
     name = establishment.nil? ? loc : establishment
     longlat = geocode["results"][0]["geometry"]["location"] rescue nil
     next if longlat.nil?
-    locations << { :address => target["formatted_address"], :name => name, :lat => longlat["lat"], :lng =>longlat["lng"] }
+    locations << { :address => target["formatted_address"], :name => name, :lat => longlat["lat"], :lng =>longlat["lng"], :datetime => time.strftime("%Y-%m-%d %H:%M:%S") }
   end
 
   content_type :json
